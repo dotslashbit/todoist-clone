626ce717dacf0b934829ca1f79b18c8d2b0282f2
Sahil Mahapatra <sahilm231199@gmail.com>
2023-12-23 10:14:27 +0530

    added auth using clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
       <h1>Hello</h1>
+      <UserButton afterSignOutUrl="/" />
     </>
   );
 }
